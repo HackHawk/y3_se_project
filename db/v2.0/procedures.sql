ae171da88c76374239383159d804561ef35d99c8
Eba-manjaro <111208298+Eba-01@users.noreply.github.com>
2023-12-27 10:39:38 +0300

    Rorganized db files and created functions

---
+++ db/v2.0/procedures.sql
@@ -0,0 +1,52 @@
+-- =============== Stored procedures v2.0 ============================================================ 
+
+CREATE OR REPLACE FUNCTION delete_book(book_id_param BIGINT)
+RETURNS VOID
+LANGUAGE plpgsql
+AS $$
+DECLARE
+    cover_page_paths TEXT[];
+    i INTEGER;
+BEGIN
+    -- Get the cover page paths for the book
+    SELECT cover_page_urls INTO cover_page_paths FROM public.books WHERE book_id = book_id_param;
+
+    -- Check if the book has been purchased
+    IF EXISTS (SELECT 1 FROM public.purchases WHERE book_id = book_id_param) THEN
+        -- Soft delete the book
+        UPDATE public.books
+        SET is_deleted = TRUE,
+            isbn = NULL,
+            synopsis = NULL,
+            amhr_synopsis = NULL,
+            genre = NULL,
+            publisher = NULL,
+            publication_date = NULL,
+            price = NULL,
+            is_hardcover = NULL,
+            average_rating = NULL,
+            quantity = NULL,
+            addition_tmstmp = NULL
+        WHERE book_id = book_id_param;
+    ELSE
+        -- Delete the book
+        DELETE FROM public.books WHERE book_id = book_id_param;
+    END IF;
+
+    -- Delete corresponding customer reviews
+    DELETE FROM public.customer_reviews WHERE book_id = book_id_param;
+
+    -- Delete prioritization records
+    DELETE FROM public.prioritized_books WHERE book_id = book_id_param;
+
+    -- Delete the cover pages from the storage bucket
+    FOR i IN 1..array_length(cover_page_paths, 1) LOOP
+        PERFORM supabase.storage.remove(cover_page_paths[i]);
+    END LOOP;
+
+    -- Clear the cover page URLs in the database
+    UPDATE public.books
+    SET cover_page_urls = NULL
+    WHERE book_id = book_id_param;
+END;
+$$;
---
+++ db/v2.0/schema.sql
@@ -0,0 +1,107 @@
+-- ===============================================================================================================
+-- Database schema v2.0
+-- ===============================================================================================================
+
+-- Contractions - C: Create, R: Read, U:Update, D:Delete
+
+-- Relations  ====================================================================================================
+
+BEGIN;
+
+CREATE TABLE
+  IF NOT EXISTS PUBLIC.genres (genre_name TEXT PRIMARY KEY);
+
+ALTER TABLE PUBLIC.genres ENABLE ROW LEVEL SECURITY;
+
+CREATE TABLE
+  IF NOT EXISTS PUBLIC.books (
+    book_id BIGSERIAL PRIMARY KEY,
+    isbn BIGINT UNIQUE,
+    title TEXT NOT NULL,
+    amhr_title TEXT,
+    authors TEXT,
+    synopsis TEXT,
+    amhr_synopsis TEXT,
+    genre TEXT,
+    publisher TEXT,
+    publication_date DATE,
+    price MONEY,
+    is_hardcover BOOLEAN,
+    average_rating DECIMAL(2, 1) DEFAULT 0,
+    quantity BIGINT,
+    addition_tmstmp TIMESTAMPTZ DEFAULT NOW () NOT NULL,
+    is_deleted BOOLEAN DEFAULT FALSE,
+    cover_page_urls TEXT [],
+    FOREIGN KEY (genre) REFERENCES PUBLIC.genres (genre_name)
+  );
+
+ALTER TABLE PUBLIC.books ENABLE ROW LEVEL SECURITY;
+
+CREATE TABLE
+  IF NOT EXISTS PUBLIC.prioritized_books (
+    book_id BIGINT PRIMARY KEY,
+    FOREIGN KEY (book_id) REFERENCES PUBLIC.books (book_id) ON DELETE CASCADE
+  );
+
+ALTER TABLE PUBLIC.prioritized_books ENABLE ROW LEVEL SECURITY;
+
+CREATE TABLE
+  IF NOT EXISTS PUBLIC.admins (
+    admin_id UUID PRIMARY KEY,
+    first_name TEXT NOT NULL,
+    last_name TEXT NOT NULL,
+    FOREIGN KEY (admin_id) REFERENCES auth.users (id) ON UPDATE CASCADE ON DELETE CASCADE
+  );
+
+ALTER TABLE PUBLIC.admins ENABLE ROW LEVEL SECURITY;
+
+CREATE TABLE
+  IF NOT EXISTS PUBLIC.sp_admin (
+    sp_admin_id UUID PRIMARY KEY,
+    first_name TEXT NOT NULL,
+    last_name TEXT NOT NULL,
+    FOREIGN KEY (sp_admin_id) REFERENCES auth.users (id) ON UPDATE CASCADE ON DELETE CASCADE
+  );
+
+ALTER TABLE PUBLIC.sp_admin ENABLE ROW LEVEL SECURITY;
+
+CREATE TABLE
+  IF NOT EXISTS PUBLIC.customers (
+    customer_id UUID PRIMARY KEY,
+    first_name TEXT NOT NULL,
+    last_name TEXT NOT NULL,
+    FOREIGN KEY (customer_id) REFERENCES auth.users (id) ON UPDATE CASCADE ON DELETE CASCADE
+  );
+
+ALTER TABLE PUBLIC.customers ENABLE ROW LEVEL SECURITY;
+
+CREATE TABLE
+  IF NOT EXISTS PUBLIC.customer_reviews (
+    customer_id UUID NOT NULL,
+    book_id BIGINT NOT NULL,
+    rating SMALLINT DEFAULT 0,
+    COMMENT TEXT,
+    PRIMARY KEY (customer_id, book_id),
+    FOREIGN KEY (customer_id) REFERENCES PUBLIC.customers (customer_id) ON DELETE CASCADE,
+    FOREIGN KEY (book_id) REFERENCES PUBLIC.books (book_id) ON DELETE CASCADE
+  );
+
+ALTER TABLE PUBLIC.customer_reviews ENABLE ROW LEVEL SECURITY;
+
+CREATE TABLE
+  IF NOT EXISTS PUBLIC.purchases (
+    customer_id UUID NOT NULL,
+    book_id BIGINT NOT NULL,
+    purchase_timestamp TIMESTAMPTZ DEFAULT NOW () NOT NULL,
+    amount MONEY NOT NULL,
+    PRIMARY KEY (customer_id, book_id, purchase_timestamp),
+    FOREIGN KEY (customer_id) REFERENCES PUBLIC.customers (customer_id) ON DELETE SET NULL,
+    FOREIGN KEY (book_id) REFERENCES PUBLIC.books (book_id)
+  );
+
+ALTER TABLE PUBLIC.purchases ENABLE ROW LEVEL SECURITY;
+
+END;
+
+
+
